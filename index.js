6109c63cb115dba17b6dbfa9db796c1bac8e2c78
Greg Connell <gconnell@160over90.com>
2021-05-25 18:29:29 -0700

    refining data load

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 import * as urls from './lib/urls.js'
 import * as io from './lib/io.js'
 
-export const load = async (config) => {
+/**
+ * Load DPS Data, 
+ * and prunes result to index 
+ * requested ssid/sid/rid 
+ * 
+	dpsConfig 		<object>		defined in Index.adParams
+		{
+			data: 		<string> 		valid JSON-string, representing hard-coded data for this ad to render
+			env: 			<string> 		origin from which to load DPS data
+									'prod'			velvet-dps.tech
+									'staging'		staging.velvet-dps.tech
+									'local'			localhost:10200
+			ssid: 		<string> 		matching a Google Spreadsheet ID
+			sid: 			<string> 		matching a Google Sheet Tab ID, expected to exist in specified ssid
+			rid: 			<string> 		matching a DPS Row ID, expected to exist in specified ssid/sid
+		}
+ */
+export const load = async (dpsConfig) => {
 	// if data has been hard-coded
-	if (config.data) {
-		return parse(config.data)
+	if (dpsConfig.data) {
+		return pruneToRow(parse(dpsConfig.data), dpsConfig.rid)
 	}
 	// otherwise make request for data
-	const url = urls.getFeedUrl(config)
+	const url = urls.getFeedUrl(dpsConfig)
 	const data = await io.request(url)
-	return parse(data)
+	return pruneToRow(parse(data), dpsConfig.rid)
 }
 
-export const parse = (data) => {
+const parse = (data) => {
 	return typeof data === 'string' ? JSON.parse(data) : data
+}
+
+const pruneToRow = (data, rid) => {
+	if (rid in data) {
+		return data[rid]
+	}
+	return data
 }
